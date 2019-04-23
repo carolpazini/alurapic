228432db7a04ad30e1469fa90d66a56c5bdadb18
Caroline Pazini Cavalcante - CCV <caroline.cavalcante@iteris.local>
2019-04-23 09:30:59 -0300

    aula 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'alurapic';
+  description = 'Cate Morland';
+  url = 'http://www.pemberley.com/janeinfo/naby2thn.gif';
+}
+
